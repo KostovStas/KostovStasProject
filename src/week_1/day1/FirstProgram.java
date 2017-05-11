0032f057af075c2e427624056c689938ab2df578
mac <mac@macs-MacBook-Pro-3.local>
2017-05-12 01:54:31 +0300

    Home task( 5 task done)

---
+++ src/week_1/day1/FirstProgram.java
@@ -0,0 +1,11 @@
+package week_1.day1;
+
+/**
+ * Created by mac on 5/8/17.
+ */
+public class FirstProgram {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world");
+    }
+}
